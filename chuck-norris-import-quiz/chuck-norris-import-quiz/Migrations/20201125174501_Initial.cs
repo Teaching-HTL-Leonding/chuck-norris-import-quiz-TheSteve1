726b149aa70f9d75133b15d1264a4e178cd729b9
VTheSteveV <56842674+VTheSteveV@users.noreply.github.com>
2020-11-25 18:48:40 +0100

    added Migrations

---
+++ chuck-norris-import-quiz/chuck-norris-import-quiz/Migrations/20201125174501_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace chucknorrisimportquiz.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "chuckNorrisJokes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChuckNorrisId = table.Column<int>(type: "int", maxLength: 40, nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(1024)", maxLength: 1024, nullable: true),
+                    Joke = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_chuckNorrisJokes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "chuckNorrisJokes");
+        }
+    }
+}
---
+++ chuck-norris-import-quiz/chuck-norris-import-quiz/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+var factory = new NorrisContextFactory();
+class ChuckNorrisJoke
+{
+	public int Id { get; set; }
+	[MaxLength(40)]
+	public int ChuckNorrisId { get; set; }
+	[MaxLength(1024)]
+	public string Url { get; set; }
+	public string Joke { get; set; }
+}
+class NorrisContext : DbContext
+{
+	public DbSet<ChuckNorrisJoke> chuckNorrisJokes { get; set; }
+
+#pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+	public NorrisContext(DbContextOptions<NorrisContext> options)
+#pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+	: base(options)
+	{ }
+}
+class NorrisContextFactory : IDesignTimeDbContextFactory<NorrisContext>
+{
+	public NorrisContext CreateDbContext(string[] args)
+	{
+		var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+		var optionsBuilder = new DbContextOptionsBuilder<NorrisContext>();
+		optionsBuilder
+			// Uncomment the following line if you want to print generated
+			// SQL statements on the console.
+			//.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()))
+			.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+		return new NorrisContext(optionsBuilder.Options);
+	}
+}
+
